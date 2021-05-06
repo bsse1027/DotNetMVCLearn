1c2c5663159926e7c3d3bdff97cb350921e31952
Junaid Mansur Ifti <bsse1027@iit.du.ac.bd>
2021-05-07 04:04:16 +0600

    Init Commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category; 
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
